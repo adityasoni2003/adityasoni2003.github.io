b160abdf3e128bdcff406ab355f8d79a596fe5b1
Aditya Soni <adityasoniprofessional@gmail.com>
2026-02-27 18:05:39 +0530

    changes in the config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://adityasoni2003.github.io',
+  output: 'static',
+
   vite: {
     plugins: [tailwindcss()]
   },
